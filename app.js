f647554c5498b53730d7786b53b2adbb92c165cc
TeomanHasgoren <teoman.hasgoren@gmail.com>
2025-02-07 16:07:10 +0300

    Progress bar, voice control, duration control,desing, music list and selected music start system added

--- app.js
+++ app.js
@@ -7,12 +7,18 @@ const play = document.querySelector("#controls #play");
 const next = document.querySelector("#controls #next");
 const duration = document.querySelector("#duration");
 const currentTime = document.querySelector("#current-time");
-const progressBar = document.querySelector("#progress-bar")
+const progressBar = document.querySelector("#progress-bar");
+const volume = document.querySelector("#volume");
+const volumeBar = document.querySelector("#volume-bar");
+const ul = document.querySelector("ul");
+
 const player = new MusicPlayer(musicList);
 
 window.addEventListener("load", () => {
     let music = player.getMusic();
     displayMusic(music);
+    displayMusicList(player.musicList);
+    isPlayingNow();
 });
 
 function displayMusic(music) {
@@ -23,44 +29,42 @@ function displayMusic(music) {
 }
 
 play.addEventListener("click", () => {
-
     const isMusicPlay = container.classList.contains("playing");
     isMusicPlay ? pauseMusic() : playMusic();
+    
 });
 
-prev.addEventListener("click", () => {
-    prevMusic();
-})
+prev.addEventListener("click", () => {prevMusic();})
 
-next.addEventListener("click", () => {
-    nextMusic();
-})
+next.addEventListener("click", () => {nextMusic();})
 
 function nextMusic(){
     player.next();
     let music = player.getMusic();
     displayMusic(music);
     playMusic();
+    isPlayingNow();
 }
 
-function prevMusic(){
+const prevMusic = () => {
     player.prev();
     let music = player.getMusic();
     displayMusic(music);
     playMusic();
+    isPlayingNow();
 }
 
 
-function pauseMusic(){
+const pauseMusic = () =>{
     container.classList.remove("playing");
-    play.classList = "fa-solid fa-play";
+    play.querySelector("i").classList = "fa-solid fa-play";
     audio.pause();
 };
 
-function playMusic(){
+const playMusic =() =>{
     container.classList.add("playing");
-    play.classList = "fa-solid fa-pause";
-    audio.play()
+    play.querySelector("i").classList = "fa-solid fa-pause";
+    audio.play();
 };
 
 const calculateTime = (toplamSaniye) => {
@@ -81,3 +85,85 @@ audio.addEventListener("timeupdate", () => {
     currentTime.textContent = calculateTime(progressBar.value)
 })
 
+progressBar.addEventListener("input", () => {
+    currentTime.textContent = calculateTime(progressBar.value);
+    audio.currentTime = progressBar.value;
+});
+
+let muteState = "unmuted";
+
+volumeBar.addEventListener("input", (e) => {
+     const value = e.target.value;
+     audio.volume = value / 100;
+
+     if(value == 0){
+        audio.muted = true;
+        muteState = "muted";
+        volume.classList = "fa-solid fa-volume-xmark";
+     }
+     else{
+        audio.muted = false;
+        muteState = "unmuted";
+        volume.classList = "fa-solid fa-volume-high";
+     }
+})
+
+volume.addEventListener("click", () => {
+    if(muteState ==="unmuted"){
+        audio.muted = true;
+        muteState = "muted";
+        volume.classList = "fa-solid fa-volume-xmark";
+        volumeBar.value = 0;
+    }else {
+        audio.muted = false;
+        muteState = "unmuted";
+        volume.classList = "fa-solid fa-volume-high";
+        volumeBar.value = 100;
+    }
+})
+
+const displayMusicList = (list) => {
+    for(let i=0; i < list.length; i++){
+        let liTag = `
+                    <li li-index='${i}' onclick="selectedMusic(this)" class="list-group-item d-flex justify-content-between align-items-center">
+                        <span>${list[i].getName()}</span>
+                        <span id="music-${i}" class="badge bg-primary rounded-pill"></span>
+                        <audio class ="music-${i}" src="mp3/${list[i].file}"></audio>
+                    </li> 
+        `;
+        ul.insertAdjacentHTML("beforeend",liTag);
+
+        let liAudioDuration = ul.querySelector(`#music-${i}`);
+        let liAudioTag = ul.querySelector(`.music-${i}`);
+
+        liAudioTag.addEventListener("loadeddata", () => {
+            liAudioDuration.innerText = calculateTime(liAudioTag.duration);
+        })
+
+        
+    }
+}
+
+const selectedMusic = (li) => {
+    player.index = li.getAttribute("li-index");
+    displayMusic(player.getMusic());
+    playMusic();
+    isPlayingNow();
+    
+}
+
+const isPlayingNow = () => {
+    for(let li of ul.querySelectorAll("li")){
+        if(li.classList.contains("playing")){
+            li.classList.remove("playing");
+        }
+
+        if(li.getAttribute("li-index") == player.index){
+            li.classList.add("playing");
+        }
+    }
+}
+
+audio.addEventListener("ended", () => {
+    nextMusic();
+})
